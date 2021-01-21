34bc8e7a56b062588cc9ecff64ed4c9a07a01bb7
钟源 <736878610@qq.com>
2021-01-21 11:13:11 +0800

    v3.0

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/TestController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.atguigu.gmall.product.controller;
 
 import com.atguigu.gmall.result.Result;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
+//@RequestMapping("/admin/product")
 public class TestController {
 
     @RequestMapping("test")
@@ -12,6 +15,12 @@ public class TestController {
         String test = "1111";
         return Result.ok(test);
     }
+
+//    @RequestMapping("/getCategory1")
+//    public Result<String> getCategory(){
+//        String test = "2222";
+//        return Result.ok(test);
+//    }
 
 
 }
