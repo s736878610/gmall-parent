ffcc56ca80520de1997b3c30de6e3e520811884b
钟源 <736878610@qq.com>
2021-01-21 15:34:46 +0800

    v3.2

--- service/service-product/src/main/java/com/atguigu/gmall/ProductApplication.java
+++ service/service-product/src/main/java/com/atguigu/gmall/ProductApplication.java
@@ -1,9 +1,11 @@
 package com.atguigu.gmall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.atguigu.gmall")
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class,args);
--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/TestController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/TestController.java
@@ -2,6 +2,7 @@ package com.atguigu.gmall.product.controller;
 
 import com.atguigu.gmall.result.Result;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,17 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 //@RequestMapping("/admin/product")
 public class TestController {
 
-    @RequestMapping("test")
+    @GetMapping("test")
     public Result<String> Test(){
         String test = "1111";
         return Result.ok(test);
     }
 
-//    @RequestMapping("/getCategory1")
-//    public Result<String> getCategory(){
-//        String test = "2222";
-//        return Result.ok(test);
-//    }
+    @GetMapping("/getCategory1")
+    public Result<String> getCategory(){
+        String test = "2222";
+        return Result.ok(test);
+    }
 
 
 }
