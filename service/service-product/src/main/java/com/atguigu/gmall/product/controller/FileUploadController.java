101a7909844a70629596ac9c4ce57b03c179e80d
钟源 <736878610@qq.com>
2021-01-23 16:20:39 +0800

    v3.4

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/FileUploadController.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.product.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("admin/product/")
+@CrossOrigin
+public class FileUploadController {
+
+
+}
